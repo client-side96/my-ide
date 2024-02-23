9845bc05352b11e3220d61398e5ce3355fe2aedb
client-side96 <dev@clientside.tech>
2024-02-23 20:18:53 +0100

    feat: add global git configuration

--- home.nix
+++ home.nix
@@ -5,9 +5,18 @@
   home.username = "frey_do";
   home.homeDirectory = "/Users/frey_do";
 
-  home.packages = [];
+  home.packages = [
+    pkgs.git
+  ];
 
   programs.home-manager.enable = true;
+
+  programs.git = {
+    enable = true;
+
+    userName = "client-side96";
+    userEmail = "dev@clientside.tech";
+  };
 }
 
 
